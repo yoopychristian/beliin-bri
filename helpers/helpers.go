2985345328f3968b3c7edec1766139b8b744a65f
yoopy <yoopy.christian@lumoshive.com>
2021-09-25 11:11:11 +0700

    update

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -4,6 +4,8 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
@@ -15,6 +17,7 @@ import (
 
 	gomail "gopkg.in/mail.v2"
 
+	generator "github.com/angelodlfrtr/go-invoice-generator"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"golang.org/x/crypto/bcrypt"
@@ -246,6 +249,7 @@ func SendMail(email, pin string) error {
 	m.SetAddressHeader("Cc", "dan@example.com", "Dan")
 	m.SetHeader("Subject", "INVOICE")
 	m.SetBody("text/html", pin)
+	m.Attach("./filepdf/out.pdf")
 	d := gomail.NewDialer("smtp.gmail.com", 587, "yoopychrs@gmail.com", "Sitohang2503")
 
 	// Send the email to Bob, Cora and Dan.
@@ -254,4 +258,73 @@ func SendMail(email, pin string) error {
 	}
 
 	return nil
+}
+
+func CreateInvoicePDF(nama, alamat_pengiriman, nama_barang, jumlah_barang, harga_barang, id_va string) {
+	doc, _ := generator.New(generator.Invoice, &generator.Options{
+		TextTypeInvoice: "INVOICE BELIIN-BRI",
+		AutoPrint:       true,
+	})
+
+	doc.SetHeader(&generator.HeaderFooter{
+		Text:       "<center>BELIIN - BRI ( USAHA MIKRO JADI LEBIH GAMPANG ).</center>",
+		Pagination: true,
+	})
+
+	doc.SetFooter(&generator.HeaderFooter{
+		Text:       "<center>BELIIN - BRI ( USAHA MIKRO JADI LEBIH GAMPANG ).</center>",
+		Pagination: true,
+	})
+
+	logoBytes, _ := ioutil.ReadFile("./logo/bantu.png")
+
+	doc.SetRef("testref")
+	doc.SetVersion("someversion")
+
+	doc.SetDescription("A description")
+	doc.SetNotes("YOUR VIRTUAL ACCOUNT : " + id_va)
+
+	time := time.Now().Format("02/01/2006")
+	doc.SetDate(string(time))
+	doc.SetPaymentTerm(string(time))
+
+	doc.SetCompany(&generator.Contact{
+		Name: "BELIIN BRI",
+		Logo: &logoBytes,
+		Address: &generator.Address{
+			Address:    "BANK BRI",
+			Address2:   "BELIIN BRI",
+			PostalCode: "75000",
+			City:       "JAKARTA PUSAT",
+			Country:    "INDONESIA",
+		},
+	})
+
+	doc.SetCustomer(&generator.Contact{
+		Name: nama,
+		Address: &generator.Address{
+			Address: alamat_pengiriman,
+			// PostalCode: "29200",
+			// City:       "Brest",
+			Country: "Indonesia",
+		},
+	})
+
+	// for i := 0; i < 2; i++ {
+	doc.AppendItem(&generator.Item{
+		Name:     nama_barang,
+		UnitCost: harga_barang,
+		Quantity: jumlah_barang,
+	})
+	// }
+
+	pdf, err := doc.Build()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := pdf.OutputFileAndClose("./filepdf/out.pdf"); err != nil {
+		log.Fatal(err)
+	}
 }
